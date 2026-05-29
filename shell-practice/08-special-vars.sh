b184da8328e7bf414a782f89f76ad79e9fc70e6b
ksnaidu <ksnaidudba@gmail.com>
2026-05-29 10:55:15 +0530

    08-special-vars.sh

--- shell-practice/08-special-vars.sh
+++ shell-practice/08-special-vars.sh
@@ -5,5 +5,5 @@ echo "Number of variables: $#"
 echo "Script name: $0"
 echo "Current Directory: $PWD"
 echo "User running this script: $USER"
-echo "Home directory of user: $HOME"
+echo "Home directory of user: $HOME
 echo "PID of the script: $$"
