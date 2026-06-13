39dc3e6c1aa57c5ab20818ae282fa89d6b228f4d
ksnaidu <ksnaidudba@gmail.com>
2026-06-13 10:56:48 +0530

    catalogue.sh added

---
+++ shell-roboshop-2/catalogue.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source ./common.sh
+app_name=catalogue
+
+check_root
+nodejs_setup
+app_setup
+systemd_setup
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo 
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Installing MongoDB Client"
+
+STATUS=$(mongosh --host mongodb.kimidi.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
+    mongosh --host mongodb.kimidi.site </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading data into MongoDB"
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi
+
+print_time
