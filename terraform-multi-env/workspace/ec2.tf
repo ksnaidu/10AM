cb20478f531e3d7375a0cc47153ca5a51df5a26b
ksnaidu <ksnaidudba@gmail.com>
2026-08-04 11:36:24 +0530

    workspace added

--- terraform-multi-env/workspace/ec2.tf
+++ terraform-multi-env/workspace/ec2.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "roboshop" {
   count                  = length(var.instances)
   ami                    = var.ami_id # left and right side names no need to be same
   instance_type          = lookup(var.instance_type, terraform.workspace)
+  subnet_id = "subnet-040e46e23d5653bd4"
   vpc_security_group_ids = [aws_security_group.allow_all.id]
 
   tags = merge(
