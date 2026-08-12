c62ebc581b6e0b24a45440dda74e84eaabbe317e
ksnaidu <ksnaidudba@gmail.com>
2026-08-12 11:06:56 +0530

    vpc

--- terraform-aws-vpc/vpc.tf
+++ terraform-aws-vpc/vpc.tf
@@ -84,5 +84,121 @@ resource "aws_subnet" "database" {
   )
 }
 
+## create Elasti-ip
+resource "aws_eip" "nat" {
+    domain   = "vpc"
+    tags = merge(
+      var.eip_tags,
+      local.common_tags,
+      {
+        Name = "${var.project}-${var.environment}"
+      }
+
+    )
+}
+
+
+##Create NAT gaeway
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+    var.nat_gateway_tags,
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}"
+    }
+
+  )
+   
+}
+
+
+## Create Route table
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.public_route_table_tags,
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-public"
+    }
+  )
+}
+
+
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.private_route_table_tags,
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-private"
+    }
+  )
+}
+resource "aws_route_table" "database" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.database_route_table_tags,
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-database"
+    }
+  )
+}
+
+
+### aws route 
+
+resource "aws_route" "public" {
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.main.id
+}
+
+resource "aws_route" "databse" {
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_nat_gateway.main.id
+}
+
+
+##Aws route table association
+
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs) #2 times run so length is using
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs) #2 times run so length is using
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidrs) #2 times run so length is using
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
+}
+
 
 
