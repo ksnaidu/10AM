dd5508730cc82dc998165163ffeb834e3fef140e
ksnaidu <ksnaidudba@gmail.com>
2026-08-19 10:39:41 +0530

    parameters

--- roboshop-infra-dev/10-sg/varaibles.tf
+++ roboshop-infra-dev/10-sg/varaibles.tf
@@ -1,23 +1,20 @@
 
 variable "project" {
-     type = string
-  
+    default = "roboshop"
 }
 
 variable "environment" {
-     type = string
-  
+    default = "dev"
 }
 
 variable "frontend_sg_name" {
     default = "frontend"
 }
 
-
 variable "frontend_sg_description" {
     default = "created sg for frontend instance"
-  
 }
+
 
 
 
