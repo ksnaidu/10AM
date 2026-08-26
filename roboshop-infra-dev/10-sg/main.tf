40677fb6ad8a334b381ec6db57a93b9f5356627d
ksnaidu <ksnaidudba@gmail.com>
2026-08-26 11:09:57 +0530

    30-vpn

--- roboshop-infra-dev/10-sg/main.tf
+++ roboshop-infra-dev/10-sg/main.tf
@@ -49,3 +49,52 @@ resource "aws_security_group_rule" "backend_alb_bastion" {
   security_group_id = module.backend_alb.sg_id
 }
 
+
+
+module "vpn" {
+    #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/ksnaidu/10AM.git//terraform-aws-securitygroup?ref=main"
+    project =  var.project
+    environment = var.environment
+
+    sg_name = "vpn"
+    sg_description = "for vpn"
+    vpc_id = local.vpc_id  ##vpc-id stroed in ssm parameter
+}
+
+resource "aws_security_group_rule" "vpn_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_1194" {
+  type              = "ingress"
+  from_port         = 1194
+  to_port           = 1194
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
+}
+
+resource "aws_security_group_rule" "vpn_943" {
+  type              = "ingress"
+  from_port         = 943
+  to_port           = 943
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.vpn.sg_id
+}
+
