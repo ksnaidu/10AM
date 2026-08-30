a48186eebe53eeea8a750088ed12ffb32eef6577
ksnaidu <ksnaidudba@gmail.com>
2026-08-30 19:26:28 +0530

    databases

--- roboshop-infra-dev/10-sg/varaibles.tf
+++ roboshop-infra-dev/10-sg/varaibles.tf
@@ -29,3 +29,20 @@ variable "mongodb_ports_vpn" {
   
 }
 
+variable "redis_ports_vpn" {
+    default = [22, 6379]
+  
+}
+
+
+variable "mysql_ports_vpn" {
+    default = [22, 3306]
+  
+}
+
+
+
+variable "rabbitmq_ports_vpn" {
+    default = [22, 5672]
+  
+}
