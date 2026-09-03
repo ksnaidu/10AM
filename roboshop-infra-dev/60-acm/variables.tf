247d1f79d9febac445ec04ac85cbccd69c4e4fc1
ksnaidu <ksnaidudba@gmail.com>
2026-09-03 10:46:31 +0530

    ACM

---
+++ roboshop-infra-dev/60-acm/variables.tf
@@ -0,0 +1,16 @@
+
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z032558618100M4EJX8X4" ##change
+}
+
+variable "zone_name" {
+    default = "kimidi.site"
+}
